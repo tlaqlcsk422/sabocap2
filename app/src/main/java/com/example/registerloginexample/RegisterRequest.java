aaa2f5032f23103991a707d3edec7f7389334b5e
simbitna <75026586+tlaqlcsk422@users.noreply.github.com>
2022-05-08 13:42:20 +0900

    ..

--- app/src/main/java/com/example/registerloginexample/RegisterRequest.java
+++ app/src/main/java/com/example/registerloginexample/RegisterRequest.java
@@ -10,7 +10,7 @@ import java.util.Map;
 public class RegisterRequest extends StringRequest {
 
     // 서버 URL 설정 ( PHP 파일 연동 )
-    final static private String URL = "http://172.30.1.23/Register.php";
+    final static private String URL = "http://192.168.219.165/Register.php";
     private Map<String, String> map;
     int num = 1000;
 
